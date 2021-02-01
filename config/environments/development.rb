b8c315885e356a4de7b20e1363e464fae2cc84ca
ravi4rails <ravionrails@gmail.com>
2021-02-01 18:17:41 +0530

    integrated devise pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -59,4 +60,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
+ end
+
